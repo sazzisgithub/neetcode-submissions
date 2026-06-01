15ce289225106fd963b947efec28fe73a5a74226
Sazzi's_Git <96123782+sazzisgithub@users.noreply.github.com>
2026-06-02 00:30:59 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = "".lower()
+        for i in s:
+            if i.isalnum():
+                st+=i
+        st=st.lower()
+        for i in range(len(st)//2):
+            if st[i]!=st[len(st)-i-1]:
+                return False
+        return True
